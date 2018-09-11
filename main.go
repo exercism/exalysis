00b5f21fe1359cc956f238977a27ce0f84bf868b
Gabriel Nelle <nelle@raceresult.com>
2018-09-11 08:59:18 +0200

    new suggester structure

--- main.go
+++ main.go
@@ -2,29 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/atotto/clipboard"
 	"github.com/tehsphinx/exalysis/suggestion"
-	"golang.org/x/tools/go/loader"
 )
 
 func main() {
-	prog := loadProg()
-
-	local := prog.Package(".")
-	sugg := suggestion.GetSuggestions(local.Pkg.Name(), local, prog)
+	sugg := suggestion.GetSuggestions()
 
 	fmt.Println(sugg)
 	clipboard.WriteAll(sugg)
-}
-
-func loadProg() *loader.Program {
-	var conf loader.Config
-	conf.Import(".")
-	prog, err := conf.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return prog
 }
