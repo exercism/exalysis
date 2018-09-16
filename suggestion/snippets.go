0974d6efe2d0573aea6528a2800c87ae61f86a8a
Gabriel Nelle <nelle@raceresult.com>
2018-09-16 16:56:22 +0200

    check if coce is formatted

--- suggestion/snippets.go
+++ suggestion/snippets.go
@@ -8,4 +8,5 @@ const (
 	goodSolution        = "This looks good!\n\nHere are some thoughts:\n"
 	interestingSolution = "This looks interesting!\n\nHere are some thoughts:\n"
 	notLinted           = "- code standards in go are very important to the community. Take a look at `golint` and run it on your code. It has some suggestions for you."
+	notFormatted        = "- code formatting in go is very important to the community. Take a look at `gofmt` and run it on your code."
 )
