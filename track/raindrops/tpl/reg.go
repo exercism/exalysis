ae4ac9e8015f7facb2e72f10082ed31dcded49db
Gabriel Nelle <nelle@raceresult.com>
2018-10-07 18:34:02 +0200

    raindrops: examine unnecessary string concatinations

--- track/raindrops/tpl/reg.go
+++ track/raindrops/tpl/reg.go
@@ -6,5 +6,6 @@ import "github.com/tehsphinx/exalysis/gtpl"
 
 //Templates to be used in the response of suggester
 var (
-	ManyLoops = gtpl.NewStringTemplate("many-loops.md", MustAsset)
+	ManyLoops       = gtpl.NewStringTemplate("many-loops.md", MustAsset)
+	ConcatNotNeeded = gtpl.NewStringTemplate("concat-not-needed.md", MustAsset)
 )
