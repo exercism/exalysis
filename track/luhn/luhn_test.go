f2fc54311155db95e71b424c7ea4661e9e394397
Gabriel Nelle <nelle@raceresult.com>
2018-11-18 12:24:47 +0100

    luhn: register luhn; introduce block suggestion in response

--- track/luhn/luhn_test.go
+++ track/luhn/luhn_test.go
@@ -17,8 +17,14 @@ var suggestTests = []struct {
 	expected   bool
 }{
 	{path: "./solutions/1", suggestion: tpl.RegexInFunc, expected: true},
+	{path: "./solutions/1", suggestion: tpl.RegexToFast, expected: true},
 	{path: "./solutions/2", suggestion: tpl.RegexInFunc, expected: false},
+	{path: "./solutions/2", suggestion: tpl.RegexToFast, expected: true},
+	{path: "./solutions/3", suggestion: tpl.RegexToFast, expected: false},
+	{path: "./solutions/4", suggestion: tpl.OneLoop, expected: true},
+	{path: "./solutions/4", suggestion: tpl.RegexToFast, expected: false},
 	{path: "./solutions/5", suggestion: tpl.RegexInFunc, expected: false},
+	{path: "./solutions/5", suggestion: tpl.RegexToFast, expected: true},
 }
 
 func Test_Suggest(t *testing.T) {
@@ -32,34 +38,6 @@ func Test_Suggest(t *testing.T) {
 		Suggest(pkg, r)
 
 		assert.Equal(t, test.expected, r.HasSuggestion(test.suggestion),
-			fmt.Sprintf("test failed: %+v", test))
-	}
-}
-
-var outroTests = []struct {
-	path       string
-	suggestion gtpl.Template
-	expected   bool
-}{
-	{path: "./solutions/1", suggestion: tpl.RegexToFast, expected: true},
-	{path: "./solutions/2", suggestion: tpl.RegexToFast, expected: true},
-	{path: "./solutions/3", suggestion: tpl.RegexToFast, expected: false},
-	{path: "./solutions/4", suggestion: tpl.OneLoop, expected: true},
-	{path: "./solutions/4", suggestion: tpl.RegexToFast, expected: false},
-	{path: "./solutions/5", suggestion: tpl.RegexToFast, expected: true},
-}
-
-func Test_Outro(t *testing.T) {
-	for _, test := range outroTests {
-		_, pkg, err := testhelper.LoadExample(test.path, "luhn")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		r := extypes.NewResponse()
-		Suggest(pkg, r)
-
-		assert.Equal(t, test.expected, r.HasOutro(test.suggestion),
 			fmt.Sprintf("test failed: %+v", test))
 	}
 }
